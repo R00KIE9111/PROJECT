94863f605bcea02d4fafe0c429b72e2cc66c514f
R00KIE9111 <leonardo.concha06@inacapmail.cl>
2026-06-24 03:32:37 -0400

    Update run.py

--- PROJECT/run.py
+++ PROJECT/run.py
@@ -226,4 +226,4 @@ def mis_historiales_view():
     return render_template("mis_historiales.html", historial=lista)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
